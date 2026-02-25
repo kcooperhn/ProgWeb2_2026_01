1590c596ac064782d755fc9c35cff4b0c9c3aabb
Joseph Cooper <kcooper.edu@gmail.com>
2026-02-24 19:20:13 -0600

    Agregando redirecciones
    Agregando flujos de redirección entre diferentes pantallas

--- demo.jsf/src/main/java/demo/jsf/uth/hn/PrimerBackbean.java
+++ demo.jsf/src/main/java/demo/jsf/uth/hn/PrimerBackbean.java
@@ -1,23 +1,54 @@
 package demo.jsf.uth.hn;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
 @Named("MiBean")
-@RequestScoped
-//@RequestScoped	->	Sirve para guardar los valores solo en esa petición. 
-//@ViewScoped 		->	Sirve para guardar los valores mientras esté en esa pantalla. 
-//@SessionScoped 	->	Sirve para guardar los valores mientras la sesión esté activa (Servidor encendido / Cache del navegador / invalidación de sesión) 
+@RequestScoped	//->	Sirve para guardar los valores solo en esa petición. 
+//@ViewScoped 		//->	Sirve para guardar los valores mientras esté en esa pantalla. 
+//@SessionScoped 	//->	Sirve para guardar los valores mientras la sesión esté activa (Servidor encendido / Cache del navegador / invalidación de sesión) 
 public class PrimerBackbean implements Serializable {
 
 	private int numero1;
 	private int numero2;
+	private String nombre;
+	private String apellido;
 	private int resultado;
+	private boolean calculoRealizado;
+	private String operacion;
 	
 	public void calcular() {
+		operacion = "Multiplicación";
+		calculoRealizado = true;
 		resultado = numero1*numero2;
+	}
+	
+	public void ejecutarCalculo() {
+		calculoRealizado = true;
+		operacion = "Logaritmo";
+		resultado = (int) (Math.log1p(numero1)+numero2);
+	}
+	
+	public String navegarADashboard() {
+		return "dashboard.xhtml";
+	}
+	
+	public String navegarAMenu() {
+		return "menu.xhtml";
+	}
+	
+	public void navegarDeRegreso() {
+		try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public int getNumero1() {
@@ -37,6 +68,34 @@ public class PrimerBackbean implements Serializable {
 	}
 	public void setResultado(int resultado) {
 		this.resultado = resultado;
+	}
+	public boolean isCalculoRealizado() {
+		return calculoRealizado;
+	}
+	public void setCalculoRealizado(boolean calculoRealizado) {
+		this.calculoRealizado = calculoRealizado;
+	}
+	public String getOperacion() {
+		return operacion;
+	}
+	public void setOperacion(String operacion) {
+		this.operacion = operacion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
 	}
 	
 }
